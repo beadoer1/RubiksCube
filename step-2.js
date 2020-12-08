94f5e0583ec482f4aaecfbfa88ea6074eda7a477
Youn-Hwan <beadoer1@gmail.com>
2020-12-08 16:41:09 +0900

    평면 큐브 동작 구현 완료(1차)
    버튼을 이용한 단순 함수 동작으로 평면 큐브 구현을 완료했다
    중복되는 문구들이 많아 객체 활용하여 줄일 필요가 있어 보인다
    한 번에 여러 문자를 받는 것과 전역변수를 줄이는 부분도 연구 필요

---
+++ step-2.js
@@ -0,0 +1,93 @@
+
+//   1. 초기 배열을 구성한다. (javadcript 3X3 배열 구성)
+var topArr = ["R", "R", "W"];
+var middleArr = ["G", "C", "W"];
+var bottomArr = ["G", "B", "B"];
+
+//   2. 초기 상태를 출력한다.
+var cube = document.getElementById('cube');
+var output = function(){
+    cube.innerHTML = topArr + "<br>"+ middleArr +"<br>"+ bottomArr;
+}
+output();
+
+//   3. 상태 변경 동작을 입력 받는다.(console 명령 구현(조건)과 함께 html 구현하는 김에 button 구성도 하고 싶다.)
+//   4. 입력 받은 조건을 구동한다
+//    * 각 명령어 별로 줄 이동이 되게끔 함수를 구성.(배열 간 이동이 고민이다.)
+//    * 명령어 별 설명
+//    U  가장 윗줄을 왼쪽으로 한 칸 밀기 RRW -> RWR
+//    U' 가장 윗줄을 오른쪽으로 한 칸 밀기 RRW -> WRR  
+//    R  가장 오른쪽 줄을 위로 한 칸 밀기 WWB -> WBW  
+//    R' 가장 오른쪽 줄을 아래로 한 칸 밀기 WWB -> BWW  
+//    L  가장 왼쪽 줄을 아래로 한 칸 밀기 RGG -> GRG (L의 경우 R과 방향이 반대임을 주의한다.)  
+//    L' 가장 왼쪽 줄을 위로 한 칸 밀기 RGG -> GGR  
+//    B  가장 아랫줄을 오른쪽으로 한 칸 밀기 GBB -> BGB (B의 경우도 U와 방향이 반대임을 주의한다.)  
+//    B' 가장 아랫줄을 왼쪽으로 한 칸 밀기 GBB -> BBG  
+//    Q  Bye~를 출력하고 프로그램을 종료한다.
+
+//   4-1. button 동작(윗줄, 아랫줄)
+var uLeft = function(){
+    var getWord = topArr.shift();
+    topArr.push(getWord);
+    output();
+};
+var uRight = function(){
+    var getWord = topArr.pop();
+    topArr.unshift(getWord);
+    output();
+};
+var bRight = function(){
+    var getWord = bottomArr.pop();
+    bottomArr.unshift(getWord);
+    output();
+};
+var bLeft = function(){
+    var getWord = bottomArr.shift();
+    bottomArr.push(getWord);
+    output();
+};
+
+//   4-2. button 동작(오른쪽, 왼쪽)
+var rUp = function(){
+    var getWord1 = topArr.pop();
+    var getWord2 = middleArr.pop();
+    var getWord3 = bottomArr.pop();
+    topArr.push(getWord2);
+    middleArr.push(getWord3);
+    bottomArr.push(getWord1);
+    output();
+};
+var rDown = function(){
+    var getWord1 = topArr.pop();
+    var getWord2 = middleArr.pop();
+    var getWord3 = bottomArr.pop();
+    topArr.push(getWord3);
+    middleArr.push(getWord1);
+    bottomArr.push(getWord2);
+    output();
+};
+var lUp = function(){
+    var getWord1 = topArr.shift();
+    var getWord2 = middleArr.shift();
+    var getWord3 = bottomArr.shift();
+    topArr.unshift(getWord2);
+    middleArr.unshift(getWord3);
+    bottomArr.unshift(getWord1);
+    output();
+};
+var lDown = function(){
+    var getWord1 = topArr.shift();
+    var getWord2 = middleArr.shift();
+    var getWord3 = bottomArr.shift();
+    topArr.unshift(getWord3);
+    middleArr.unshift(getWord1);
+    bottomArr.unshift(getWord2);
+    output();
+};
+//   4-3. button 동작(종료 버튼)
+var q = function(){
+    cube.innerHTML = "Bye~~!";
+}
+
+//   5. 변동된 조건을 출력한다. (html 에 text 형태 및 console 모두 뜨도록 하자.)
+//   -> 버튼에 적용 완료
