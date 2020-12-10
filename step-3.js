4de86bc09c2ffdd9531252eb263342a1971def9b
Youn-Hwan <beadoer1@gmail.com>
2020-12-10 20:52:26 +0900

    큐브 회전 동작 객체 구현
    1. 주 회전면 회전(시계, 반시계) 적용
    2. 회전면 주변 4개면 이동(Row, Column 주고 받기) 구현 적용
    ※ 순서를 역으로 넣어줘야하는 경우의 수가 있어서 cubeMoveSide 객체에 메소드 추가

--- step-3.js
+++ step-3.js
@@ -65,8 +65,14 @@ var output = {
             outArr[i].innerHTML = strArr[i][0] + "<br>" + strArr[i][1] + "</br>" + strArr[i][2]
         }
         return;
+    },
+    result : function(){
+        var id = output.get(totalId);
+        var resultStr = output.arrToStr(totalSurf);
+        output.printStr(id, resultStr);
     }
 };
+
 // 3. 상태 변경 동작을 입력 받는다.(한 줄 입력 명령 구현(조건)과 함께 html 구현하는 김에 button 구성 해보기.)
 // 4. 입력 받은 조건을 구동한다.(큐브 작동에 대한 코드 규현)
 // 4-1. 조작 면에 적용되는 움직임 구현
@@ -78,12 +84,12 @@ var cubeMoveMain = {
                getObj.push(obj[totalArr[i]][j]);
             }
         };
-        var resultObj = {
+        Obj = {
             arr0 : [getObj[2],getObj[5],getObj[8]],
             arr1 : [getObj[1],getObj[4],getObj[7]],
-            arr2 : [getObj[0],getObj[3].getObj[6]]
+            arr2 : [getObj[0],getObj[3],getObj[6]]
         };
-        return resultObj;
+        return Obj;
     },
     turnRight : function(obj){
         var getObj = [];
@@ -100,6 +106,7 @@ var cubeMoveMain = {
         return resultObj;
     }
 };
+
 // 4-2. 조작 면 주변 4면에 적용되는 움직임을 구현
 var cubeMoveSide = {
     getRow : function(obj, num){ // num은 0(first row) 혹은 2(third row)를 입력.
@@ -117,50 +124,368 @@ var cubeMoveSide = {
         objTo[totalArr[num]] = valueFrom;
         return;
     },
+    pushRowRev : function(valueFrom, objTo, num){ //역으로 넣어주는 경우 필요
+        var result = [];
+        while(valueFrom.length != 0){
+            var getValue = valueFrom.pop();
+            result.push(getValue);
+        }
+        objTo[totalArr[num]] = result;
+        return;
+    },
     pushColumn : function(valueFrom, objTo, num){ // num은 0(first row) 혹은 2(third row)를 입력.
         for(i = 0; i < 3; i++){
             objTo[totalArr[i]][num] = valueFrom[i];
         }
         return;
+    },
+    pushColumnRev : function(valueFrom, objTo, num){ //역으로 넣어주는 경우 필요
+        for(i = 0; i < 3; i++){
+            objTo[totalArr[i]][num] = valueFrom[2-i];
+        }
+        return;
     }
 };
+
 // 4-3. 6면 조작에 대한 함수 구현
 var cubeMove = {
     top : function(){
-        console.log("Top Move");
+        cubeMoveMain.turnRight(surfTop);
+
+        var turn = [surfFront, surfLeft, surfBack, surfRight]; 
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            getArr.push(cubeMoveSide.getRow(turn[i],0));
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            cubeMoveSide.pushRow(getArr[j], turn[j], 0);
+        }
+
+        console.log("Top Reverse Move");
+        output.result();
+        return;
     },
     bottom : function(){
+        cubeMoveMain.turnRight(surfBottom);
+
+        var turn = [surfFront, surfRight, surfBack, surfLeft];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            getArr.push(cubeMoveSide.getRow(turn[i],2));
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            cubeMoveSide.pushRow(getArr[j], turn[j], 2); 
+        }
         console.log("Bottom Move");
+        output.result();
+        return;
     },
     left : function(){
+        cubeMoveMain.turnRight(surfLeft); 
+
+        var turn = [surfTop, surfFront, surfBottom, surfBack];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i <= 2){
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            } else{
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){ 
+            if(j <= 1){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 0);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            } else{
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            }
+        }
         console.log("Left Move");
+        output.result();
+        return;
     },
     right : function(){
+        cubeMoveMain.turnRight(surfRight);
+
+        var turn = [surfBottom, surfFront, surfTop, surfBack];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i <= 2){
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            } else{
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            }
+        };
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j <= 1){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 2);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            } else{
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            }
+        };
         console.log("Right Move");
+        output.result();
+        return;
     },
     front : function(){
+        cubeMoveMain.turnRight(surfFront); 
+
+        var turn = [surfTop, surfRight, surfBottom, surfLeft]; 
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i === 0){
+                getArr.push(cubeMoveSide.getRow(turn[i],2));
+            } else if(i === 1){
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            } else if(i === 2){
+                getArr.push(cubeMoveSide.getRow(turn[i],0));
+            } else {
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j === 0){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 0);
+            } else if(j === 1){
+                cubeMoveSide.pushRowRev(getArr[j], turn[j], 0);
+            } else if(j === 2){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 2);
+            } else{
+                cubeMoveSide.pushRowRev(getArr[j], turn[j], 2);
+            }
+        }
+
         console.log("Front Move");
+        output.result();
+        return;
     },
     back : function(){
+        cubeMoveMain.turnRight(surfBack); 
+
+        var turn = [surfTop, surfLeft, surfBottom, surfRight]; 
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i === 0){
+                getArr.push(cubeMoveSide.getRow(turn[i],0));
+            } else if(i === 1){
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            } else if(i === 2){
+                getArr.push(cubeMoveSide.getRow(turn[i],2));
+            } else {
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j === 0){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            } else if(j === 1){
+                cubeMoveSide.pushRow(getArr[j], turn[j], 2);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            } else{
+                cubeMoveSide.pushRow(getArr[j], turn[j], 0);
+            }
+        }
+
         console.log("Back Move");
+        output.result();
+        return;
     },
     topRev : function(){
+        cubeMoveMain.turnLeft(surfTop);
+
+        var turn = [surfFront, surfRight, surfBack, surfLeft];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            getArr.push(cubeMoveSide.getRow(turn[i],0));
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            cubeMoveSide.pushRow(getArr[j], turn[j], 0);
+        }
         console.log("Top Reverse Move");
+        output.result();
+        return;
     },
     bottomRev : function(){
-        console.log("Bottom Reverse Move");
+        cubeMoveMain.turnLeft(surfBottom);
+
+        var turn = [surfFront, surfLeft, surfBack, surfRight];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            getArr.push(cubeMoveSide.getRow(turn[i],2));
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            cubeMoveSide.pushRow(getArr[j], turn[j], 2);
+        }
+        console.log("Bottom Move");
+        output.result();
+        return;
     },
     leftRev : function(){
+        cubeMoveMain.turnLeft(surfLeft);
+
+        var turn = [surfBottom, surfFront, surfTop, surfBack];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i <= 2){
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            } else{
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            }
+        };
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j <= 1){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 0);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            } else{
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            }
+        };
         console.log("Left Reverse Move");
+        output.result();
+        return;
     },
+
     rightRev : function(){
+        cubeMoveMain.turnLeft(surfRight);
+
+        var turn = [surfTop, surfFront, surfBottom, surfBack];
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i <= 2){
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            } else{
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j <= 1){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 2);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            } else{
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            }
+        }
         console.log("Right Reverse Move");
+        output.result();
+        return;
     },
     frontRev : function(){
+        cubeMoveMain.turnLeft(surfFront); 
+
+        var turn = [surfTop, surfLeft, surfBottom, surfRight]; 
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i === 0){
+                getArr.push(cubeMoveSide.getRow(turn[i],2));
+            } else if(i === 1){
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            } else if(i === 2){
+                getArr.push(cubeMoveSide.getRow(turn[i],0));
+            } else {
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j === 0){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 2);
+            } else if(j === 1){
+                cubeMoveSide.pushRow(getArr[j], turn[j], 0);
+            } else if(j === 2){
+                cubeMoveSide.pushColumnRev(getArr[j], turn[j], 0);
+            } else{
+                cubeMoveSide.pushRow(getArr[j], turn[j], 2);
+            }
+        }
+
         console.log("Front Reverse Move");
+        output.result();
+        return;
     },
     backRev : function(){
+        cubeMoveMain.turnLeft(surfBack); 
+
+        var turn = [surfTop, surfRight, surfBottom, surfLeft]; 
+        var getArr = [];
+
+        for(var i = 0; i < turn.length; i++){
+            if(i === 0){
+                getArr.push(cubeMoveSide.getRow(turn[i],0));
+            } else if(i === 1){
+                getArr.push(cubeMoveSide.getColumn(turn[i],2));
+            } else if(i === 2){
+                getArr.push(cubeMoveSide.getRow(turn[i],2));
+            } else {
+                getArr.push(cubeMoveSide.getColumn(turn[i],0));
+            }
+        }
+
+        turn.push(turn.shift());
+
+        for(var j = 0; j < turn.length; j++){
+            if(j === 0){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 2);
+            } else if(j === 1){
+                cubeMoveSide.pushRowRev(getArr[j], turn[j], 2);
+            } else if(j === 2){
+                cubeMoveSide.pushColumn(getArr[j], turn[j], 0);
+            } else{
+                cubeMoveSide.pushRowRev(getArr[j], turn[j], 0);
+            }
+        }
+
         console.log("Back Reverse Move");
+        output.result();
+        return;
     }
 }
 var quitBtn = {
@@ -169,10 +494,4 @@ var quitBtn = {
     }
 }
 // 5. 변동된 조건을 출력한다. (html 에 text 형태로 구현.)
-var main = function(){
-    var id = output.get(totalId);
-    var resultStr = output.arrToStr(totalSurf);
-    output.printStr(id, resultStr);
-}
-
-main();
+output.result();
