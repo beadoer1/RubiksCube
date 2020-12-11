ec5928a0d7fbb265fbdd374a35e663321385fea7
Youn-Hwan <beadoer1@gmail.com>
2020-12-11 23:36:51 +0900

    평면 큐브의 동작을 객체화, 동작 함수 최소화
    1. 같은 방향으로 동작하는 객체 내의 함수로 만들어 코드 간소화

--- step-2.js
+++ step-2.js
@@ -1,81 +1,85 @@
 
 //   1. 초기 배열을 구성한다. (javadcript 3X3 배열 구성)
-var topArr = ["R", "R", "W"];
-var middleArr = ["G", "C", "W"];
-var bottomArr = ["G", "B", "B"];
-
+var cube = {
+    arr0 : ["R", "R", "W"],
+    arr1 : ["G", "C", "W"],
+    arr2 : ["G", "B", "B"]
+};
 //   2. 초기 상태를 출력한다.
-var cube = document.getElementById('cube');
 var output = function(){
-    cube.innerHTML = topArr + "<br>"+ middleArr +"<br>"+ bottomArr;
-}
-output();
-
+    var cubeArea = document.getElementById('cubeArea');
+    cubeArea.innerHTML = cube.arr0 + "<br>"+ cube.arr1 +"<br>"+ cube.arr2;
+};
 //   3. 상태 변경 동작을 입력 받는다.(console 명령 구현(조건)과 함께 html 구현하는 김에 button 구성도 하고 싶다.)
 //   4. 입력 받은 조건을 구동한다.
-//   4-1. button 동작(윗줄, 아랫줄)
-var uLeft = function(){
-    var getWord = topArr.shift();
-    topArr.push(getWord);
-    output();
+//   4-1. 동작 객체 (왼쪽,오른쪽 이동(윗줄, 아랫줄), 위,아래 이동(왼줄, 오른줄))
+
+var cubeMove = {
+    left : function(arr){
+        var getWord = arr.shift();
+        arr.push(getWord);
+        output();
+    },
+    right : function(arr){
+        var getWord = arr.pop();
+        arr.unshift(getWord);
+        output();
+    },
+    up : function(obj, colNum){
+        var getArr = [];
+        var arrs = ["arr0","arr1","arr2"];
+
+        for (var i = 0; i < 3; i++){
+            getArr.push(obj[arrs[i]][colNum]);
+        };
+        obj["arr0"][colNum] = getArr[1];
+        obj["arr1"][colNum] = getArr[2];
+        obj["arr2"][colNum] = getArr[0];
+        output();
+    },
+    down : function(obj, colNum){
+        var getArr = [];
+        var arrs = ["arr0","arr1","arr2"];
+
+        for (var i = 0; i < 3; i++){
+            getArr.push(obj[arrs[i]][colNum]);
+        };
+        obj["arr0"][colNum] = getArr[2];
+        obj["arr1"][colNum] = getArr[0];
+        obj["arr2"][colNum] = getArr[1];
+        output();
+    }
+}
+//   4-2. button 동작 함수 구
+var tLeft = function(){
+    cubeMove.left(cube.arr0);
 };
-var uRight = function(){
-    var getWord = topArr.pop();
-    topArr.unshift(getWord);
-    output();
+var tRight = function(){
+    cubeMove.right(cube.arr0);
 };
 var bRight = function(){
-    var getWord = bottomArr.pop();
-    bottomArr.unshift(getWord);
-    output();
+    cubeMove.right(cube.arr2);
 };
 var bLeft = function(){
-    var getWord = bottomArr.shift();
-    bottomArr.push(getWord);
-    output();
+    cubeMove.left(cube.arr2);
 };
-
-//   4-2. button 동작(오른쪽, 왼쪽)
 var rUp = function(){
-    var getWord1 = topArr.pop();
-    var getWord2 = middleArr.pop();
-    var getWord3 = bottomArr.pop();
-    topArr.push(getWord2);
-    middleArr.push(getWord3);
-    bottomArr.push(getWord1);
-    output();
+    cubeMove.up(cube, 2);
 };
 var rDown = function(){
-    var getWord1 = topArr.pop();
-    var getWord2 = middleArr.pop();
-    var getWord3 = bottomArr.pop();
-    topArr.push(getWord3);
-    middleArr.push(getWord1);
-    bottomArr.push(getWord2);
-    output();
+    cubeMove.down(cube, 2);
 };
 var lUp = function(){
-    var getWord1 = topArr.shift();
-    var getWord2 = middleArr.shift();
-    var getWord3 = bottomArr.shift();
-    topArr.unshift(getWord2);
-    middleArr.unshift(getWord3);
-    bottomArr.unshift(getWord1);
-    output();
+    cubeMove.up(cube, 0);
 };
 var lDown = function(){
-    var getWord1 = topArr.shift();
-    var getWord2 = middleArr.shift();
-    var getWord3 = bottomArr.shift();
-    topArr.unshift(getWord3);
-    middleArr.unshift(getWord1);
-    bottomArr.unshift(getWord2);
-    output();
+    cubeMove.down(cube, 0);
 };
+
 //   4-3. button 동작(종료 버튼)
 var q = function(){
-    cube.innerHTML = "Bye~~!";
-}
+    cubeArea.innerHTML = "Bye~~!";
+};
 
 //   5. 변동된 조건을 출력한다. (html 에 text 형태 및 console 모두 뜨도록 하자.)
-//   -> 버튼에 적용 완료
+output();
