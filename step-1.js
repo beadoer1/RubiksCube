3bdeaf18a74c73210c64f55827ce3dd8dbbb6f1d
Youn-Hwan <beadoer1@gmail.com>
2020-12-11 20:19:17 +0900

    조건 확인 절차 객체로 분류 및 정수 확인 조건 추가
    1. 조건 확인 절차 객체(check)로 분류
    2. 숫자 입력에 대한 정수 여부 확인 조건 추가(check.num 내부에 추가)

--- step-1.js
+++ step-1.js
@@ -1,10 +1,3 @@
-// input 창 내에 적힌 텍스트 불러오기
-// 3가지 요소 배열로 나누기
-// 단어 요소 배열로 다시 나누기
-// 단어 요소 저장 배열 방향, 칸 수 만큼 밀어내기
-// 다시 배열로 저장
-// 배열 -> 문자열 변경 후 출력
-
 var input = {
     text : function(){
         t = document.getElementById('in').value;
@@ -12,38 +5,46 @@ var input = {
     }
 }
 
-var resultArr = {
-    right : function(arr, num){
-        if(Number(num) >= -100 && Number(num) <= 0){
-            for(var i = Number(num); i < 0; i++){
-                var w = arr.shift();
-                arr.push(w);
+var check = {
+
+    resultErr : true,
+
+    num : function(num){
+        if(num - Math.floor(num) === 0){
+            if(Number(num) >= -100 && Number(num) < 0){
+                return true;
+            } else if(Number(num) >= 0 && Number(num) < 100){
+                return false;
+            } else{
+                this.resultErr = false;
             }
-        } else if(Number(num) > 0 && Number(num) < 100){
-            for(var i = 0; i < Number(num); i++){
-                var w = arr.pop();
-                arr.unshift(w);
-            }
+        } else{
+            this.resultErr = false;
+        }
+    },
+    direction : function(value){
+        if(value === "r" || value === "R"){
+            return true;
+        } else if(value === "l" || value === "L"){
+            return false;
         } else {
-            document.write("잘못된 값을 입력하였습니다.");
-            return;
+            this.resultErr = false;
+        }
+    }
+};
+
+var movePuzzle = {
+    right : function(arr, num){
+        for(var i = 0; i < Number(num); i++){
+            var w = arr.pop();
+            arr.unshift(w);
         }
         return arr;
     },
     left : function(arr, num){
-        if(Number(num) >= -100 && Number(num) <= 0){
-            for(var i = 0; i < Number(num); i++){
-                var w = arr.pop();
-                arr.unshift(w);
-            }
-        } else if(Number(num) > 0 && Number(num) < 100){
-            for(var i = 0; i < Number(num); i++){
-                var w = arr.shift();
-                arr.push(w);
-            }
-        } else {
-            document.write("잘못된 값을 입력하였습니다.");
-            return;
+        for(var i = 0; i < Number(num); i++){
+            var w = arr.shift();
+            arr.push(w);
         }
         return arr;
     }
@@ -51,8 +52,13 @@ var resultArr = {
 
 var output = {
     printResult : function(result){
-        var out = document.getElementById('output');
-        out.innerHTML = "최종 결과값은 " + result + " 입니다.";
+        var out = document.getElementById("output");
+        if (check.resultErr === true){
+            out.innerHTML = "최종 결과값은 " + result + " 입니다.";
+        } else {
+            out.innerHTML = "값을 잘못 입력 하였습니다.(조건 참고)";
+            check.resultErr = true;
+        }
     }    
 }
 
@@ -60,14 +66,11 @@ var main = function() {
     var mainText = input.text();
     var sepArr = mainText.split(" ");
     var sepTextArr = sepArr[0].split("");
-    if(sepArr[1] === "r" || sepArr[1] === "R") {
-        resultArr.right(sepTextArr, sepArr[2]);
-    } else if(sepArr[1] === "l" || sepArr[1] === "L"){
-        resultArr.left(sepTextArr, sepArr[2]);
+    if(check.num(sepArr[1]) + check.direction(sepArr[2]) === 1){
+        movePuzzle.right(sepTextArr, sepArr[1]);
     } else {
-        document.write("잘못된 값을 입력하였습니다.");
-        return;
-    };
+        movePuzzle.left(sepTextArr, sepArr[1]);
+    }
     var result = sepTextArr.join("");
     output.printResult(result);
 }
